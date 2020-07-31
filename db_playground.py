60bfd0c6083d7dcc8e48a270d1a83bd977128d47
hajimalung.baba <hajimalung.baba@kony.com>
2020-07-31 22:21:38 +0530

    fixing read from csv

--- db_playground.py
+++ db_playground.py
@@ -3,4 +3,9 @@ def write_to_csv(dict):
 	with open('database.csv',mode='a',newline='') as database:
 		csv_writer = csv.writer(database,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
 		csv_writer.writerow(dict)
-	pass
+
+
+def read_from_csv():
+	with open('database.csv',mode='r',newline='') as database:
+		csv_reader = csv.reader(database,delimiter=',',quotechar='"')
+		return [row for row in csv_reader]
